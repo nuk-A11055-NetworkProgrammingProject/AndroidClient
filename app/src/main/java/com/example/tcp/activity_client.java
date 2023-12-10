ce7dfbd8c43439a071222f2d8d03f599c09df958
Nat Su <95582835+natsu5510@users.noreply.github.com>
2023-12-11 04:01:53 +0800

    完成聊天訊息顯示

--- app/src/main/java/com/example/tcp/activity_client.java
+++ app/src/main/java/com/example/tcp/activity_client.java
@@ -1,5 +1,6 @@
 package com.example.tcp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,12 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tcp.message.MsgItem;
+import com.example.tcp.message.MyAdapter;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -21,32 +25,43 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
 
 public class activity_client extends AppCompatActivity {
-    private String msgFromGroupChat;
-    private ScrollView scrollView;
-    private TextView receivedMessages;
-    private TextView TEST;
+    private String[] splitReceivedMsg;
+    private RecyclerView recyclerView;
+    private MyAdapter myAdapter;
+    private ArrayList<MsgItem> msgArray;
     private EditText editText;
     private FrameLayout layoutSend;
     String username;
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
-    private final List<String> pendingMessages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+
         Intent intent = getIntent();
         username = intent.getStringExtra("username");
         editText = findViewById(R.id.editText);
         layoutSend = (android.widget.FrameLayout) findViewById(R.id.layoutSend);
+        recyclerView = findViewById(R.id.recyclerView);
+
+
+        msgArray = new ArrayList<>();
+        msgArray.add(new MsgItem("tom", "hello, world", "1312"));
+        msgArray.add(new MsgItem("tom", "hello, world", "1312"));
+        msgArray.add(new MsgItem("tom", "hello, world", "1312"));
+        msgArray.add(new MsgItem("tom", "hello, world", "1312"));
+
+        myAdapter = new MyAdapter(activity_client.this, msgArray);
+
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity_client.this));
 
         String groupName = intent.getStringExtra("group_name");
-//        TEST.setText(groupName);
         socket = SocketConnection.get().getSocket();
         try {
             bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
@@ -71,69 +86,48 @@ public class activity_client extends AppCompatActivity {
         });
     }
 
-    private class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder>{
-        class ViewHolder extends RecyclerView.ViewHolder{
-            TextView msgName, msgContent, msgTime;
-            public ViewHolder(@NonNull View itemView) {
-                super(itemView);
-                msgName = itemView.findViewById(R.id.msgName);
-                msgContent = itemView.findViewById(R.id.msgContent);
-                msgTime = itemView.findViewById(R.id.msgTime);
-            }
-        }
-
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item_layout, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
-        }
-    }
-
-    public void listenForMessage(){
+    public void listenForMessage() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (socket.isConnected()){
-                    try{
-
-                        msgFromGroupChat = bufferedReader.readLine();
-                        runOnUiThread(new Runnable() {
+                while (socket.isConnected()) {
+                    try {
+                        // 接收伺服器broadcast的訊息
+                        String receivedMsg = bufferedReader.readLine(); // read broadcast from server
+                        receivedMsg += ":1213";   // set test time
+                        // 切割出訊息內容(名子、訊息內容、時間)
+                        splitReceivedMsg = receivedMsg.split(":");  // split message
+                        // 加入訊息至msgArray
+                        msgArray.add(new MsgItem(splitReceivedMsg));
+                        runOnUiThread(new Runnable() {  // 刷新聊天介面
                             @Override
                             public void run() {
-//                                receivedMessages.append(msgFromGroupChat  + "\n");
-//                                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                                // 刷新聊天訊息
+                                myAdapter.notifyItemInserted(msgArray.size() - 1);
+                                // 訊息超出螢幕時，自動往下滾動
+                                recyclerView.scrollToPosition(msgArray.size() - 1);
                             }
                         });
-                    }catch (IOException e){
-                        closeEverything(socket,bufferedReader,bufferedWriter);
+                    } catch (IOException e) {
+                        closeEverything(socket, bufferedReader, bufferedWriter);
                     }
                 }
             }
         }).start();
     }
-    public void closeEverything(Socket socket,BufferedReader bufferedReader,BufferedWriter bufferedWriter){
-        try{
-            if(bufferedReader != null){
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if (bufferedReader != null) {
                 bufferedReader.close();
             }
-            if(socket != null){
+            if (socket != null) {
                 socket.close();
             }
-            if(bufferedWriter != null){
+            if (bufferedWriter != null) {
                 bufferedWriter.close();
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -144,7 +138,7 @@ public class activity_client extends AppCompatActivity {
             public void run() {
                 try {
                     if (bufferedWriter != null) {
-                        bufferedWriter.write(username + " : " + message);
+                        bufferedWriter.write(username + ":" + message);
                         bufferedWriter.newLine();
                         bufferedWriter.flush();
                     }
@@ -154,6 +148,4 @@ public class activity_client extends AppCompatActivity {
             }
         }).start();
     }
-
-
 }
